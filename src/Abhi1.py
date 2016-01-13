aef65d56cea744311099e710309c68e4330ca3e6
Wayne Paffhausen <wpaffhausen@ranzal.com>
2016-01-13 07:49:54 -0500

    Test upload of the Abhi Project

---
+++ src/Abhi1.py
@@ -0,0 +1,27 @@
+strRecord = "2014,513,172000,NULL,NULL,NULL,B,-330.06,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00"
+strFind = "513"
+#strField = strRecord[6:15]
+#strField = int(strField)
+#strFA = strRecord[68:70]
+
+#arrValues = strRecord.split(",")
+#if len(arrValues[1]) == 3 :
+#  print "three"
+  
+#print strRecord.split(",")[1]
+
+if strRecord.find(strFind) <> -1 :
+  print "hello"
+
+#floatAmount = float(arrValues[7])
+
+#if not strFA :
+#  print "Blank"
+#else:
+#  print str(strField) + '.' + str(strRecord[68:70])
+  
+  
+#print float("00001")
+
+#def tPrint(strRecord):
+#  print strRecord
